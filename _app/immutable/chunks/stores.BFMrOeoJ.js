ac237916d4a0815af1b33902dbc3d92526dec9c7
emir-masinovic <emir3710549@gmail.com>
2024-07-08 00:39:27 +0300

    sveltekit removed

--- _app/immutable/chunks/stores.BFMrOeoJ.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./entry.DP7gb3Of.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
